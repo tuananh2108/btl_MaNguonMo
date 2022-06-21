a2fc63a55a8ce360af98bd8ab08cf096fa4f46bd
dung-20cm <laithedung28563@gmail.com>
2022-06-21 21:13:38 +0700

    build front-end

--- App/Models/User.php
+++ App/Models/User.php
@@ -7,28 +7,12 @@ use \App\Token;
 use \App\Mail;
 use \Core\View;
 
-/**
- * User model
- *
- * PHP version 7.0
- */
+
 class User extends \Core\Model
 {
 
-    /**
-     * Error messages
-     *
-     * @var array
-     */
     public $errors = [];
 
-    /**
-     * Class constructor
-     *
-     * @param array $data  Initial property values (optional)
-     *
-     * @return void
-     */
     public function __construct($data = [])
     {
         foreach ($data as $key => $value) {
@@ -36,11 +20,6 @@ class User extends \Core\Model
         };
     }
 
-    /**
-     * Save the user model with the current property values
-     *
-     * @return boolean  True if the user was saved, false otherwise
-     */
     public function save()
     {
         $this->validate();
@@ -48,10 +27,6 @@ class User extends \Core\Model
         if (empty($this->errors)) {
 
             $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
-
-//            $token = new Token();
-//            $hashed_token = $token->getHash();
-//            $this->activation_token = $token->getValue();
 
             date_default_timezone_set('Asia/Ho_Chi_Minh');
             $created_at = date('Y-m-d H:m:s');
@@ -74,52 +49,40 @@ class User extends \Core\Model
         return false;
     }
 
-    /**
-     * Validate current property values, adding valiation error messages to the errors array property
-     *
-     * @return void
-     */
     public function validate()
     {
         // Name
         if ($this->name == '') {
-            $this->errors[] = 'Name is required';
+            $this->errors[] = 'Tên bắt buộc.';
         }
 
         // email address
         if (filter_var($this->email, FILTER_VALIDATE_EMAIL) === false) {
-            $this->errors[] = 'Invalid email';
+            $this->errors[] = 'Email không hợp lệ!';
         }
         if (static::emailExists($this->email, $this->id ?? null)) {
-            $this->errors[] = 'email already taken';
+            $this->errors[] = 'Email đã tồn tại.';
         }
 
         // Password
         if (isset($this->password)) {
 
             if (strlen($this->password) < 6) {
-                $this->errors[] = 'Please enter at least 6 characters for the password';
+                $this->errors[] = 'Vui lòng nhập ít nhất 6 ký tự cho mật khẩu!';
             }
 
             if (preg_match('/.*[a-z]+.*/i', $this->password) == 0) {
-                $this->errors[] = 'Password needs at least one letter';
+                $this->errors[] = 'Mật khẩu cần ít nhất một chữ cái.';
             }
 
             if (preg_match('/.*\d+.*/i', $this->password) == 0) {
-                $this->errors[] = 'Password needs at least one number';
+                $this->errors[] = 'Mật khẩu cần ít nhất một chữ số.';
             }
 
         }
     }
 
-    /**
-     * See if a user record already exists with the specified email
-     *
-     * @param string $email email address to search for
-     * @param string $ignore_id Return false anyway if the record found has this ID
-     *
-     * @return boolean  True if a record already exists with the specified email, false otherwise
-     */
+
     public static function emailExists($email, $ignore_id = null)
     {
         $user = static::findByEmail($email);
@@ -133,13 +96,6 @@ class User extends \Core\Model
         return false;
     }
 
-    /**
-     * Find a user model by email address
-     *
-     * @param string $email email address to search for
-     *
-     * @return mixed User object if found, false otherwise
-     */
     public static function findByEmail($email)
     {
         $sql = 'SELECT * FROM users WHERE email = :email';
@@ -155,15 +111,6 @@ class User extends \Core\Model
         return $stmt->fetch();
     }
 
-    /**
-     //* Authenticate a user by email and password.
-     * Authenticate a user by email and password. User account has to be active.
-     *
-     * @param string $email email address
-     * @param string $password password
-     *
-     * @return mixed  The user object or false if authentication fails
-     */
     public static function authenticate($email, $password)
     {
         $user = static::findByEmail($email);
@@ -178,13 +125,7 @@ class User extends \Core\Model
         return false;
     }
 
-    /**
-     * Find a user model by ID
-     *
-     * @param string $id The user ID
-     *
-     * @return mixed User object if found, false otherwise
-     */
+
     public static function findByID($id)
     {
         $sql = 'SELECT * FROM users WHERE id = :id';
@@ -200,12 +141,7 @@ class User extends \Core\Model
         return $stmt->fetch();
     }
 
-    /**
-     * Remember the login by inserting a new unique token into the remembered_logins table
-     * for this user record
-     *
-     * @return boolean  True if the login was remembered successfully, false otherwise
-     */
+
     public function rememberLogin()
     {
         $token = new Token();
@@ -227,13 +163,7 @@ class User extends \Core\Model
         return $stmt->execute();
     }
 
-    /**
-     * Send password reset instructions to the user specified
-     *
-     * @param string $email The email address
-     *
-     * @return void
-     */
+
     public static function sendPasswordReset($email)
     {
         $user = static::findByEmail($email);
@@ -248,11 +178,7 @@ class User extends \Core\Model
         }
     }
 
-    /**
-     * Start the password reset process by generating a new token and expiry
-     *
-     * @return void
-     */
+
     protected function startPasswordReset()
     {
         $token = new Token();
@@ -276,11 +202,7 @@ class User extends \Core\Model
         return $stmt->execute();
     }
 
-    /**
-     * Send password reset instructions in an email to the user
-     *
-     * @return void
-     */
+
     protected function sendPasswordResetEmail()
     {
         $url = 'http://' . $_SERVER['HTTP_HOST'] . '/password/reset/' . $this->password_reset_token;
@@ -291,13 +213,7 @@ class User extends \Core\Model
         Mail::send($this->email, 'Password reset', $text, $html);
     }
 
-    /**
-     * Find a user model by password reset token and expiry
-     *
-     * @param string $token Password reset token sent to user
-     *
-     * @return mixed User object if found and the token hasn't expired, null otherwise
-     */
+
     public static function findByPasswordReset($token)
     {
         $token = new Token($token);
@@ -327,13 +243,7 @@ class User extends \Core\Model
         }
     }
 
-    /**
-     * Reset the password
-     *
-     * @param string $password The new password
-     *
-     * @return boolean  True if the password was updated successfully, false otherwise
-     */
+
     public function resetPassword($password)
     {
         $this->password = $password;
@@ -363,11 +273,7 @@ class User extends \Core\Model
         return false;
     }
 
-    /**
-     * Send an email to the user containing the activation link
-     *
-     * @return void
-     */
+
     public function sendActivationEmail()
     {
         $url = 'http://' . $_SERVER['HTTP_HOST'] . '/signup/activate/' . $this->activation_token;
@@ -378,13 +284,7 @@ class User extends \Core\Model
         Mail::send($this->email, 'Account activation', $text, $html);
     }
 
-    /**
-     * Activate the user account with the specified activation token
-     *
-     * @param string $value Activation token from the URL
-     *
-     * @return void
-     */
+
     public static function activate($value)
     {
         $token = new Token($value);
@@ -403,13 +303,7 @@ class User extends \Core\Model
         $stmt->execute();
     }
     
-    /**
-     * Update the user's profile
-     *
-     * @param array $data Data from the edit profile form
-     *
-     * @return boolean  True if the data was updated, false otherwise
-     */
+
     public function updateProfile($data)
     {
         $this->name = $data['name'];
